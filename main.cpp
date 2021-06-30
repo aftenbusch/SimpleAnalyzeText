a77d8b96e2ac04f1aa2d99f30e501cb19524d6d6
aftenbusch <63379458+aftenbusch@users.noreply.github.com>
2021-06-30 18:41:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "textanalysis.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	bool debug_mode{false};
+	
+	// read in and check command line arguments:
+    vector<string> commandLineArguments{};
+    for( int i=0; i<argc; ++i )
+    {
+        commandLineArguments.push_back(argv[i]);
+    }
+	if( argc < 2 || commandLineArguments[1] == "--help" )
+	{
+		cout << "Usage: " << commandLineArguments[0] << " <input file>\n";
+		cout << "       --help  Displays this message.\n";
+	}
+	if(debug_mode)
+	{
+		cout << "*** debug: ";
+		for( const auto &a : commandLineArguments )
+			cout << a << " ";
+		cout << "\n";
+	}
+
+	// perform the text analysis on the input:
+	string inputFile = commandLineArguments[1];
+	TextAnalysis textAnalysis{inputFile};
+	
+	textAnalysis.outputAnalysis();
+	
+	return 0;
+}
---
+++ textanalysis.h
@@ -0,0 +1,27 @@
+#ifndef TEXTANALYSIS_H
+#define TEXTANALYSIS_H
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class TextAnalysis
+{
+	public:
+
+		explicit TextAnalysis( string infile );
+		void outputAnalysis( ostream &output = cout );
+
+	private:
+
+		vector<string> originalText;
+		vector<string> cleanedText;
+		multiset<string> words;
+		map<string,int> wordCount;
+};
+
+#endif // TEXTANALYSIS_H
